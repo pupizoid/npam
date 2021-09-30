1c9fa7bd75358b31545b2e353a317b0dfd598cb2
pupizoid <pupizoid77@gmail.com>
2021-09-30 12:16:30 +0300

    change PAM service

--- test/Npam.Tests/NpamTestsCommon.cs
+++ test/Npam.Tests/NpamTestsCommon.cs
@@ -4,7 +4,7 @@ namespace Npam.Test
     {
         public const string TestUsernameGood = "userxyz";
         public const string TestUsernameBad = "userabc";
-        public const string TestService = "passwd";
+        public const string TestService = "login";
         public const string TestPassword = "pwd123";
         public const string TestNewPassword = "pwd1234";
     }
